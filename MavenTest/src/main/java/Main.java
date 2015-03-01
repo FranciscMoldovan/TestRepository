12b1df5e9997d8dd29ab7163b4cedf4254c0665e
FranciscMoldovan <franciscmoldovan93@gmail.com>
2015-03-01 18:08:02 +0200

    Added Project

---
+++ MavenTest/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.sql.Statement;
+
+/**
+ * Created by Francisc on 3/1/2015.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("bei bota");
+    }
+}
